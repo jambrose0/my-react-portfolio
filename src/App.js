af52b27f770fee5d62492a4fc4082c0c587dbc24
jambrose0 <jambrose0518@gmail.com>
2022-05-22 22:37:43 -0400

    reverted to previous commit

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Nav from "./components/Nav";
 import About from "./components/About";
 import Footer from "./components/Footer";
 import Portfolio from "./components/Portfolio";
-import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="bg-mtn w-screen fixed absolute fixed text-white">
       <Nav></Nav>
-      <main className="">
+      <main className="m-5">
         <About></About>
         <Portfolio></Portfolio>
+<<<<<<< HEAD
         <Contact></Contact>
+=======
+>>>>>>> parent of c13db4a (contact form appears and validates email)
       </main>
       <footer>
         <Footer></Footer>
